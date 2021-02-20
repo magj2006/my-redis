557af58945c4d6fe5c8c25a5d0ca135d71246739
Marshall <magj2006@gmail.com>
2021-02-20 21:53:07 +0500

    Hello Tokio

---
+++ Readme.md
@@ -0,0 +1,6 @@
+## All code  is from tutorial of Tokio official document
+
+1. Hello Tokio
+    - async fn
+    - #[totio::main]
+    - .await
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use bytes::Bytes;
+use mini_redis::{client, Result};
+
+#[tokio::main]
+pub async fn main() -> Result<()> {
+    let mut client = client::connect("127.0.0.1:6379").await?;
+
+    client.set("hello", Bytes::from("world")).await?;
+
+    let result = client.get("hello").await?;
+
+    println!("get value from server; result = {:?}", result);
+
+    Ok(())
 }
